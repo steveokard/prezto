6905f02040a68e240a6abb9d060e25df853b5e0e
Stevie Card <steveokard@gmail.com>
2019-03-30 16:39:41 -0400

    typo

--- modules/fasd/init.zsh
+++ modules/fasd/init.zsh
@@ -55,4 +55,4 @@ function fasd_cd {
 # Changes the current working directory interactively.
 alias j='fasd_cd -i'
 # Changes the current working directory, same functionality as j in autojump.
-alias z='fasd_cd
+alias z='fasd_cd'
